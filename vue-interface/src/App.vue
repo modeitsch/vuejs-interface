e624a6644a3e5a9decd042a7b339ed2302b51dca
Moshe Deitsch <me@moshedeitsch.com>
2020-03-22 20:06:55 +0200

    main component

--- vue-interface/src/App.vue
+++ vue-interface/src/App.vue
@@ -1,20 +1,34 @@
 <template>
   <div id="main-app" class="container">
-    <h4>{{title}}</h4>
-    <font-awesome-icon icon='plus' />Add Appointment
+    <appointment-list @remove='removeItem' :appointments='appointments' />
   </div>
 </template>
 
 <script>
-
+import AppointmentList from "./components/Appointments";
+import axios from "axios";
+import _ from 'lodash';    
 export default {
-  name: 'MainApp',
-    data: function() {
-      return { 
+  name: "MainApp",
+  data: function() {
+    return {
       title: "Appointment List",
-      };
-    },
-
-  };
+      appointments: []
+    };
+  },
+  mounted() {
+    axios
+      .get("./data/appointments.json")
+      .then(response => (this.appointments = response.data));
+    console.log(this.appointments);
+  },
+  methods: {
+    removeItem: function(apt){
+      this.appointments = _.without(this.appointments, apt);
+    }
+  },
+  components: {
+    AppointmentList
+  }
+};
 </script>
-
